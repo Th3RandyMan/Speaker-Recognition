cfd73263677cc2a693b7875ddc647f1acbce1260
Randall Fowler <rlfowler@ucdavis.edu>
2024-03-15 11:38:43 -0700

    Updating small changes

--- tests.py
+++ tests.py
@@ -10,21 +10,34 @@ twelve_train_files = glob(f'{data_folder}\Twelve Train\*.wav')
 zero_test_files = glob(f'{data_folder}\Zero Test\*.wav')
 zero_train_files = glob(f'{data_folder}\Zero Train\*.wav')
 
-# Create codelibraries
-twelve_codelibrary = CodeLibrary()
-twelve_codelibrary.fillLibrary("Codebooks/Best Twelve")
-zero_codelibrary = CodeLibrary()
-zero_codelibrary.fillLibrary("Codebooks/Best Zero")
+twelve_zero_codelibrary = CodeLibrary()
+twelve_zero_codelibrary.fillLibrary("Codebooks/Twelve Zero")
+#twelve_zero_train_files = twelve_train_files + zero_train_files
+twelve_zero_test_files = twelve_test_files + zero_test_files
 
-# Predict the test files
-for filename in twelve_test_files:
-    name = filename.split('\\')[-1][:-4]
-    print(f'Twelve Test: {name}')
-    print(f'Predicted: {twelve_codelibrary.predict(filename)}')
-    print()
+# for filename in twelve_zero_test_files:
+#     name = filename.split('\\')[-1][:-4]
+#     print(f'Twelve Zero Train: {name}')
+#     print(f'Predicted: {twelve_zero_codelibrary.predict(filename,N = 1024, M = 409, n_mfcc = 40)}')
+#     print()
 
-for filename in zero_test_files:
-    name = filename.split('\\')[-1][:-4]
-    print(f'Zero Test: {name}')
-    print(f'Predicted: {zero_codelibrary.predict(filename)}')
-    print()
+print(f"Accuracy: {twelve_zero_codelibrary.getAccuracy(twelve_zero_test_files,N = 1024, M = 409, n_mfcc = 40)}")
+
+# # Create codelibraries
+# twelve_codelibrary = CodeLibrary()
+# twelve_codelibrary.fillLibrary("Codebooks/Best Twelve")
+# zero_codelibrary = CodeLibrary()
+# zero_codelibrary.fillLibrary("Codebooks/Best Zero")
+
+# # Predict the test files
+# for filename in twelve_test_files:
+#     name = filename.split('\\')[-1][:-4]
+#     print(f'Twelve Test: {name}')
+#     print(f'Predicted: {twelve_codelibrary.predict(filename)}')
+#     print()
+
+# for filename in zero_test_files:
+#     name = filename.split('\\')[-1][:-4]
+#     print(f'Zero Test: {name}')
+#     print(f'Predicted: {zero_codelibrary.predict(filename)}')
+#     print()
